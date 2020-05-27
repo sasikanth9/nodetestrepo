2f31d45bb7f1e06abab54e3baaf6fbc899758434
sasikanth <sasikanth.b.009@gmail.com>
2020-05-28 03:11:06 +0530

    f

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const port = process.env.PORT || 3003;
+const mongoUri = "mongodb+srv://sasikanth:dbcpass123@dbccluster0-nn6hg.mongodb.net/test?retryWrites=true&w=majority";
+
+//body-parser middleware
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+
+//setup
+mongoose.connect(
+    mongoUri,
+    {useNewUrlParser: true,useUnifiedTopology: true},
+    (err)=>{
+        if(err) console.log("Could'nt connect to db\n",err);
+        else{console.log('Connected to db');}
+    }
+);
+
+// app.set('view engine','ejs');
+// app.use(express.static(__dirname + "/public"));
+
+var myarr = []
+var i=0
+while(i<10){
+myarr[i] = {
+    a : "title "+i,
+    b : "desc "+i
+}
+i++
+}
+console.log(myarr);
+
+
+app.get('/',(req,res) => {
+    res.json(myarr);
+})
+
+
+app.listen(port,() => {console.log(`running on ${port}`)});
