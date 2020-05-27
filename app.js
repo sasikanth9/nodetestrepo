b1fb8662bf3e79bca24e68c327ecb8544995f19d
sasikanth <sasikanth.b.009@gmail.com>
2020-05-28 04:49:05 +0530

    8

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ console.log(myarr);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "access-control-allow-origin,Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 
